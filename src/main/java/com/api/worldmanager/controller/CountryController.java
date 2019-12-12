92a38b306c3d147ce9153b6c9b7c2b3447e2a6c5
Omar-Aouini <aouiniomar@outlook.it>
2019-12-12 14:08:25 +0100

    refactored for lambdas in service

--- src/main/java/com/api/worldmanager/controller/CountryController.java
+++ src/main/java/com/api/worldmanager/controller/CountryController.java
@@ -6,13 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class CountryController
 {
-
-
 
     @Autowired
     ICountryServiceImpl countryservice;
--- src/main/java/com/api/worldmanager/service/ICountryServiceImpl.java
+++ src/main/java/com/api/worldmanager/service/ICountryServiceImpl.java
@@ -9,9 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 import java.util.stream.Collectors;
 
@@ -34,41 +32,54 @@ public class ICountryServiceImpl implements ICountryService {
     public List<CountryDTO> getAllCountries()
     {
         return countryRepository.findAll()
-                                .stream()
+                                .parallelStream()
                                 .map(country -> modelMapper.map(country, CountryDTO.class))
                                 .collect(Collectors.toList());
     }
 
 
+
     @Override
     public CountryDTO getCountryByCountryCode(String countrycode) throws Exception, EntityNotFoundException {
-        if (isValidCode(countrycode)) {
-            try {
-                Optional<Country> country = countryRepository.findByCode(countrycode);
-                return modelMapper.map(country.get(), CountryDTO.class);
-            } catch (EntityNotFoundException e) {
+        if (isValidCode(countrycode))
+        {
+            try
+            {
+                return countryRepository.findByCode(countrycode)
+                                        .map(c -> modelMapper.map(c, CountryDTO.class))
+                                        .get();
+            }
+            catch (EntityNotFoundException e)
+            {
                 throw new EntityNotFoundException("entity by countrycode " + countrycode + " not found!");
             }
-        } else
+        }
+        else
             throw new Exception("countrycode not valid or null!");
     }
 
     @Override
-    public void addCountry(CountryDTO countryDto) throws Exception {
-        if (EntityValidator.getInstance().isCountryValid(countryDto)) {
+    public void addCountry(CountryDTO countryDto) throws Exception
+    {
+        if (EntityValidator.getInstance().isCountryValid(countryDto))
+        {
             Country country = modelMapper.map(countryDto, Country.class);
             countryRepository.save(country);
-        } else
+        }
+        else
             throw new Exception("entity not valid for saving! " + countryDto.toString());
     }
 
     @Override
     public void updateCountry(String countrycode, CountryDTO countryDto) throws Exception {
-        if (isValidCode(countrycode)) {
-            if (EntityValidator.getInstance().isCountryValid(countryDto)) {
+        if (isValidCode(countrycode))
+        {
+            if (EntityValidator.getInstance().isCountryValid(countryDto))
+            {
                 Country country = modelMapper.map(countryDto, Country.class);
                 countryRepository.save(country);
-            } else
+            }
+            else
                 throw new Exception("entity not valid for saving! " + countryDto.toString());
         } else
             throw new Exception("countrycode " + countrycode + " is not valid!");
@@ -76,56 +87,59 @@ public class ICountryServiceImpl implements ICountryService {
 
     @Override
     public CountryDTO getCountryByName(String name) throws Exception {
-        if (name != null && !name.equals("")) {
-            try {
-                Optional<Country> country = countryRepository.findByName(name);
-                return modelMapper.map(country.get(), CountryDTO.class);
-            } catch (EntityNotFoundException e) {
+        if (name != null && !name.equals(""))
+        {
+            try
+            {
+                return countryRepository.findByName(name)
+                                        .map(c -> modelMapper.map(c, CountryDTO.class))
+                                        .get();
+            }
+            catch (EntityNotFoundException e)
+            {
                 throw new EntityNotFoundException("entity by name " + name + " not found!");
             }
-        } else
+        }
+        else
             throw new Exception("name is not valid or null!");
     }
 
     @Override
     public List<CountryDTO> getAllCountriesPopulationGreaterThan(Integer popnumber) throws Exception {
-        if (popnumber != null && popnumber >= 0) {
-            List<Country> list = countryRepository.findByPopulationGreaterThan(popnumber);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
-        } else
+        if (popnumber != null && popnumber >= 0)
+        {
+            return countryRepository.findByPopulationGreaterThan(popnumber)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
+        }
+        else
             throw new Exception("popnumber is null or not valid!");
     }
 
     @Override
     public List<CountryDTO> getAllCountriesPopulationLessThan(Integer popnumber) throws Exception {
-        if (popnumber != null && popnumber >= 0) {
-            List<Country> list = countryRepository.findByPopulationLessThan(popnumber);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
+        if (popnumber != null && popnumber >= 0)
+        {
+            return countryRepository.findByPopulationLessThan(popnumber)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
+            
         } else
             throw new Exception("popnumber is null or not valid!");
     }
 
     @Override
     public List<CountryDTO> getAllCountriesByRegion(String region) throws Exception {
-        if (region != null && !region.equals("")) {
-            List<Country> list = countryRepository.findByRegion(region);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
-        } else
+        if (region != null && !region.equals(""))
+        {
+            return countryRepository.findByRegion(region)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
+        }
+        else
             throw new Exception("region is null or not valid!");
 
     }
@@ -143,85 +157,78 @@ public class ICountryServiceImpl implements ICountryService {
 
     @Override
     public List<CountryDTO> getAllCountriesByContinent(String continent) throws Exception {
-        if (continent != null && !continent.equals("")) {
-            List<Country> list = countryRepository.findByContinent(continent);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
-        } else
+        if (continent != null && !continent.equals(""))
+        {
+            return countryRepository.findByContinent(continent)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
+        }
+        else
             throw new Exception("continent is null or not valid!");
     }
 
     @Override
     public List<CountryDTO> getAllCountriesByGovernmentForm(String govern) throws Exception {
-        if (govern != null && !govern.equals("")) {
-            List<Country> list = countryRepository.findByGovernmentForm(govern);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
+        if (govern != null && !govern.equals(""))
+        {
+            return countryRepository.findByGovernmentForm(govern)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
         } else
             throw new Exception("government form is null or not valid!");
     }
 
     @Override
     public List<CountryDTO> findCountriesBySurfaceAreaLessThan(double surfacearea) throws Exception {
-        if (surfacearea > 0.0) {
-            List<Country> list = countryRepository.findBySurfaceAreaLessThan(surfacearea);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
-        } else
+        if (surfacearea > 0.0)
+        {
+            return countryRepository.findBySurfaceAreaLessThan(surfacearea)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
+        }
+        else
             throw new Exception("surfacearea negative!");
     }
 
     @Override
     public List<CountryDTO> findCountriesBySurfaceAreaGreaterThan(double surfacearea) throws Exception {
-        if (surfacearea > 0.0) {
-            List<Country> list = countryRepository.findBySurfaceAreaGreaterThan(surfacearea);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
-        } else
+        if (surfacearea > 0.0)
+        {
+            return countryRepository.findBySurfaceAreaGreaterThan(surfacearea)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
+        }
+        else
             throw new Exception("surfacearea negative!");
     }
 
     @Override
     public List<CountryDTO> getAllCountriesLifeExpectancyLessThan(double yearsoflife) throws Exception {
-        if (yearsoflife > 0.0) {
-            List<Country> list = countryRepository.findByLifeExpectancyLessThan(yearsoflife);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
-        } else
+        if (yearsoflife > 0.0)
+        {
+            return countryRepository.findByLifeExpectancyLessThan(yearsoflife)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
+        }
+        else
             throw new Exception("years of life number negative!");
     }
 
     @Override
     public List<CountryDTO> getAllCountriesLifeExpectancyGreaterThan(double yearsoflife) throws Exception {
-        if (yearsoflife > 0.0) {
-            List<Country> list = countryRepository.findByLifeExpectancyGreaterThan(yearsoflife);
-            List<CountryDTO> listDto = new ArrayList<>();
-            for (Country c : list) {
-                CountryDTO countryDto = modelMapper.map(c, CountryDTO.class);
-                listDto.add(countryDto);
-            }
-            return listDto;
-        } else
+        if (yearsoflife > 0.0)
+        {
+           return countryRepository.findByLifeExpectancyGreaterThan(yearsoflife)
+                                    .parallelStream()
+                                    .map(country -> modelMapper.map(country, CountryDTO.class))
+                                    .collect(Collectors.toList());
+        }
+        else
             throw new Exception("years of life number negative!");
     }
 
