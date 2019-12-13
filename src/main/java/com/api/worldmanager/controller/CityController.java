c8e30b51440e24efb7b66a4a77b242e5393cea57
Omar-Aouini <aouiniomar@outlook.it>
2019-12-13 15:46:54 +0100

    refactored citycontroller

--- src/main/java/com/api/worldmanager/controller/CityController.java
+++ src/main/java/com/api/worldmanager/controller/CityController.java
@@ -2,6 +2,8 @@ package com.api.worldmanager.controller;
 
 import com.api.worldmanager.dto.CityDTO;
 import com.api.worldmanager.service.ICityServiceImpl;
+import com.api.worldmanager.validation.EntityValidator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +37,7 @@ public class CityController {
 
         return new ResponseEntity<>(list,HttpStatus.OK);
     }
+
     
 
     @GetMapping("/{id}")
@@ -50,55 +53,117 @@ public class CityController {
     }
 
     @GetMapping("/name/{name}")
-    public CityDTO getCityByName(@PathVariable String name) throws Exception {
-        return cityService.getCityByName(name);
+    public ResponseEntity<?> getCityByName(@PathVariable String name) throws Exception
+    {
+        RepresentationModel<CityDTO> city = cityService.getCityByName(name);
+
+        if(city==null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(city,HttpStatus.OK);
     }
 
     @GetMapping("/countrycode/{countrycode}")
-    public CityDTO getCityByCountryCode(@PathVariable String countrycode) throws Exception {
-        return cityService.getCityByCountryCode(countrycode);
+    public ResponseEntity<?> getCityByCountryCode(@PathVariable String countrycode) throws Exception
+    {
+        RepresentationModel<CityDTO> city = cityService.getCityByCountryCode(countrycode);
+
+        if(city==null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(city,HttpStatus.OK);
     }
 
     @GetMapping("/populationlessthan/{popnumber}")
-    public List<CityDTO> getAllCitiesPopulationLessThan(@PathVariable Integer popnumber) throws Exception {
-        return cityService.getAllCitiesPopulationLessThan(popnumber);
+    public ResponseEntity<?> getAllCitiesPopulationLessThan(@PathVariable Integer popnumber) throws Exception
+    {
+        List<CityDTO> list = cityService.getAllCitiesPopulationLessThan(popnumber).parallelStream()
+                                        .map(c-> c.add(new Link(linktemplate + c.getId()).withSelfRel(),
+                                                       new Link(linktemplate).withRel("cities")))          
+                                        .collect(Collectors.toList());
+
+        if(list.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(list,HttpStatus.OK);
     }
 
     @GetMapping("/populationgreaterthan/{popnumber}")
-    public List<CityDTO> getAllCitiesPopulationGreaterThan(@PathVariable Integer popnumber) throws Exception {
-        return cityService.getAllCitiesPopulationGreaterThan(popnumber);
+    public ResponseEntity<?> getAllCitiesPopulationGreaterThan(@PathVariable Integer popnumber) throws Exception
+    {
+        List<CityDTO> list = cityService.getAllCitiesPopulationGreaterThan(popnumber).parallelStream()
+                                        .map(c-> c.add(new Link(linktemplate + c.getId()).withSelfRel(),
+                                                       new Link(linktemplate).withRel("cities")))          
+                                        .collect(Collectors.toList());
+
+        if(list.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(list,HttpStatus.OK);
     }
 
     @GetMapping("/district/{district}")
-    public List<CityDTO> getAllCitiesByDistrict(@PathVariable String district) throws Exception {
-        return cityService.getAllCitiesByDistrict(district);
+    public ResponseEntity<?> getAllCitiesByDistrict(@PathVariable String district) throws Exception {
+       List<CityDTO> list = cityService.getAllCitiesByDistrict(district).parallelStream()
+                                        .map(c-> c.add(new Link(linktemplate + c.getId()).withSelfRel(),
+                                                       new Link(linktemplate).withRel("cities")))          
+                                        .collect(Collectors.toList());
+
+        if(list.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(list,HttpStatus.OK);
     }
 
 
     //POST
     @PostMapping("/new")
-    public void addNewCity(@RequestBody CityDTO city) throws Exception {
+    public ResponseEntity<?> addNewCity(@RequestBody CityDTO city) throws Exception
+    {
+        if(city==null || ! EntityValidator.getInstance().isCityValid(city))
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
         cityService.addCity(city);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
     //PUT
     @PutMapping("/{id}")
-    public void updateCity(@PathVariable Integer id, @RequestBody CityDTO city) throws Exception {
-        cityService.updateCity(id,city);
+    public ResponseEntity<?> updateCity(@PathVariable Integer id, @RequestBody CityDTO city) throws Exception
+    {
+                        //not
+        if(city==null || !EntityValidator.getInstance().isCityValid(city) || id==null || id <=0)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        cityService.addCity(city);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
     //DELETE
     @DeleteMapping("/{id}")
-    public void deleteCity(@PathVariable Integer id) throws Exception {
-        cityService.deleteCity(id);
+    public ResponseEntity<Object> deleteCity(@PathVariable Integer id) throws Exception
+    {
+        if(id==null || id <= 0)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+            cityService.deleteCity(id);
+            return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/api/cities")
-    public void deleteAllCities()
+    public ResponseEntity<Object> deleteAllCities()
     {
         cityService.deleteAllCities();
+        List<CityDTO> list = cityService.getAllCities();
+
+        if(list.isEmpty())
+            return new ResponseEntity<>(HttpStatus.OK);
+        else
+            return new ResponseEntity<>(list,HttpStatus.BAD_REQUEST);
+        
+
     }
 
 }
